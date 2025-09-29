6f75141da34559d5a10a9040236b886b6bb2d57e
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-09-30 01:11:32 +0530

    scaffold: Integrated user register route

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,9 @@
-import React from "react";
-import UberImage from "../assets/UberImage.png";
-import { Link } from "react-router-dom";
+import React from 'react'
+
 const Home = () => {
   return (
-    <div>
-      <div className="h-screen bg-[url(https://images.unsplash.com/photo-1619059558110-c45be64b73ae?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center  w-full bg-red-400 flex justify-between flex-col">
-        <img src={UberImage} alt="" className="w-24"/>
-        <div className="bg-white pb-7 px-4 py-4">
-          <h2 className="text-2xl font-bold">Getting Started with Uber</h2>
-          <Link to='/login' className="flex items-center justify-center w-full bg-black text-white py-3 rounded-sm mt-4">Continue</Link>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <div>Home</div>
+  )
+}
 
-export default Home;
- 
+export default Home
--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { UserDataContext } from "../context/UserContext";
 
 const UserSignup = () => {
   const [email, setEmail] = useState("");
@@ -8,13 +10,34 @@ const UserSignup = () => {
   const [lastname, setLastname] = useState("");
   const [userData, setUserData] = useState({});
 
-  const submitHandler = (e) => {
+  const navigate = useNavigate();
+
+  const { user, setUser } = React.useContext(UserDataContext);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setUserData({
+    // setUserData({
+    //   fullName: { firstName: firstName, lastname: lastname },
+    //   email: email,
+    //   password: password,
+    // });
+
+    const newUser = {
       fullName: { firstName: firstName, lastname: lastname },
       email: email,
       password: password,
-    });
+    };
+
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/users/register`,
+      newUser
+    );
+
+    if (response.status === 201) {
+      const data = response.data;
+      setUser(data.user);
+      navigate("/home");
+    }
     console.log(userData);
     setEmail("");
     setPassword("");
@@ -76,7 +99,7 @@ const UserSignup = () => {
             placeholder="Create password"
           />
           <button className="bg-[#111] text-white mb-3 font-semibold rounded-md px-4 py-2 w-full text-lg placeholder:text-base">
-            Login
+            Create Account
           </button>
         </form>
         <Link
@@ -89,7 +112,9 @@ const UserSignup = () => {
 
       <div>
         <p className="text-xs leading-tight tracking-tight font-semibold">
-          This site is protected by reCAPTCHA and the <span className="underline">Google Privacy Policy</span> and <span className="underline">Terms of Service apply</span>.
+          This site is protected by reCAPTCHA and the{" "}
+          <span className="underline">Google Privacy Policy</span> and{" "}
+          <span className="underline">Terms of Service apply</span>.
         </p>
       </div>
     </div>
